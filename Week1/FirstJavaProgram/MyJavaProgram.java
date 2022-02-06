f1854e5e196920067e3f55eefddda3335f77ce9f
BashaTaylor <bashataylor.22@gmail.com>
2022-02-06 18:01:39 -0500

    Updating

---
+++ Week1/FirstJavaProgram/MyJavaProgram.java
@@ -0,0 +1,8 @@
+public class MyJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Basha Taylor");
+        System.out.println("I am 10 years old");
+        System.out.println("My hometown is Southington, CT");
+        System.out.println("Java rocks!");
+    }
+}
