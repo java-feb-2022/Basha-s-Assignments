b5155d97fde7b9da989e46aff51b394c84a9e96a
BashaTaylor <bashataylor.22@gmail.com>
2022-02-13 16:28:46 -0500

    Sunday update

--- Week1/AlfredBot/AlfredTest.java
+++ Week1/AlfredBot/AlfredTest.java
@@ -14,13 +14,9 @@ public class AlfredTest {
         String testGuestGreeting = alfredBot.guestGreeting("Beth Kane", "evening");
         String testDateAnnouncement = alfredBot.dateAnnouncement();
         
-        String alexisTest = alfredBot.respondBeforeAlexis(
-                            "Alexis! Play some low-fi beats.");
-        String alfredTest = alfredBot.respondBeforeAlexis(
-            "I can't find my yo-yo. Maybe Alfred will know where it is.");
-        String notRelevantTest = alfredBot.respondBeforeAlexis(
-            "Maybe that's what Batman is about. Not winning. But failing.."
-        );
+        String alexisTest = alfredBot.respondBeforeAlexis("Alexis! Play some low-fi beats.");
+        String alfredTest = alfredBot.respondBeforeAlexis("I can't find my yo-yo. Maybe Alfred will know where it is.");
+        String notRelevantTest = alfredBot.respondBeforeAlexis("Maybe that's what Batman is about. Not winning. But failing..");
         
         // Print the greetings to test.
         System.out.println(testGreeting);
