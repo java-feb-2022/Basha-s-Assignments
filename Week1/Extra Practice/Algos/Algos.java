d298a9a4b885c05d786c7bb925c91f7931305de3
BashaTaylor <bashataylor.22@gmail.com>
2022-02-13 16:18:38 -0500

    update

---
+++ Week1/Extra Practice/Algos/Algos.java
@@ -0,0 +1,13 @@
+public class Algos {
+    public static void main(String[] args) {
+        //create an array - 1-10
+        int[] numbers={1,2,3,4,5,6,7,8,9,10};
+
+        //Reverse the array
+        
+
+        //Print the array 1-10
+        
+        System.out.print{numbers};
+    }
+}
